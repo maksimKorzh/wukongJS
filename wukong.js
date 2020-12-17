113fbb781b5aff1973794d835da9408458f74a49
Maksim Korzh <freesoft.for.people@gmail.com>
2020-12-17 15:40:21 +0200

    output best move from last iteration if time is up

--- wukong.js
+++ wukong.js
@@ -223,7 +223,7 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
       pieceList[piece] = 0;
     
     // reset piece list
-    for (var index = 0; index < 13 * 10; index++)
+    for (var index = 0; index < pieceList.pieces.length; index++)
       pieceList.pieces[index] = 0;
     
     // associate pieces with squares and count material
@@ -943,7 +943,7 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
 
         // evaluate material
         score += materialWeights[piece];
-        (piece >= 1 && piece <= 6) ? score += pst[square]: score -= pst[square];//pst[mirrorSquare[square]];
+        (piece >= 1 && piece <= 6) ? score += pst[square]: score -= pst[mirrorSquare[square]];
       }
     }
     
@@ -979,7 +979,8 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
   // search  constants
   const maxPly = 64;
   const infinity = 50000;
-  const mateScore = 49000;
+  const mateValue = 49000;
+  const mateScore = 48000;
   
   // PV table
   var pvTable = new Array(maxPly * maxPly);
@@ -1074,7 +1075,7 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
       var score = -quiescence(-beta, -alpha);
       takeBack();
       
-      if (timing.stopped == 1) return 0;
+      if (timing.stopped == 1) {console.log('quiescence done');return 0;}
       if (score > alpha) {
         alpha = score;
         if (score >= beta) return beta;
@@ -1090,45 +1091,44 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
     
     let score = 0;
 
-    //if ((searchPly && isRepetition()) || fifty >= 100) return 0;
-    //if ((nodes & 2047 ) == 0) checkTime();
+    if ((searchPly && isRepetition()) || fifty >= 100) return 0;
+    if ((nodes & 2047 ) == 0) checkTime();
     if (depth == 0) { nodes++; return evaluate(); }
     
     let legalMoves = 0;
     let inCheck = isSquareAttacked(kingSquare[side], side ^ 1);
-    //if (inCheck) depth++;
-    
-    
+    if (inCheck) depth++;
+
     let moveList = [];
     generateMoves(moveList);
     
     for (let count = 0; count < moveList.length; count++) {
-      //sortMove(count, moveList);
+      sortMove(count, moveList);
       let move = moveList[count].move;
       if (makeMove(move) == 0) continue;
       legalMoves++;
       score = -negamax(-beta, -alpha, depth - 1);
       takeBack();
       
-      //if (timing.stopped == 1) return 0;
+      if (timing.stopped == 1) return 0;
       if (score > alpha) {
         alpha = score;
         
         // store history moves
-        //if (getMoveCapture(move) == 0)
-          //historyMoves[board[getMoveSource(move)] * 128 + getMoveTarget(move)] += depth;
+        if (getMoveCapture(move) == 0)
+          historyMoves[board[getMoveSource(move)] * 128 + getMoveTarget(move)] += depth;
         
         // store PV move
-        /*pvTable[searchPly * 64 + searchPly] = move;
+        pvTable[searchPly * 64 + searchPly] = move;
         for (var nextPly = searchPly + 1; nextPly < pvLength[searchPly + 1]; nextPly++)
           pvTable[searchPly * 64 + nextPly] = pvTable[(searchPly + 1) * 64 + nextPly];
         pvLength[searchPly] = pvLength[searchPly + 1]
-        */
+        
         if (score >= beta) {
           // store killer moves
           if (getMoveCapture(move) == 0) {
-            //killerMoves[maxPly + searchPly] = killerMoves[searchPly];
-            //killerMoves[searchPly] = move;
+            killerMoves[maxPly + searchPly] = killerMoves[searchPly];
+            killerMoves[searchPly] = move;
           }
           
           return beta;
@@ -1137,52 +1137,60 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
     }
     
     // checkmate or stalemate
-    /*if (legalMoves == 0) {
-      if (inCheck) return -mateScore + searchPly;
+    if (legalMoves == 0) {
+      if (inCheck) return -mateValue + searchPly;
       else return 0;
-    }*/
+    }
 
     return alpha;
   }
   
   function searchPosition(depth) {
-    negamax(-infinity, infinity, depth);
-    console.log(nodes)
-    return
-    
-    var start = new Date().getTime();
-    var score = 0;
+    let start = new Date().getTime();
+    let score = 0;
     
     clearSearch();
-
-    // iterative deepening
-    for (var current_depth = 1; current_depth <= depth; current_depth++) {
-      if (timing.stopped == 1) break;
-      score = negamax(-infinity, infinity, current_depth);
-
-      // if PV is available
-      //if (pvLength[0]) {
-        //if (score > -mate_value && score < -mate_score)
-          //console.log('Score: mate in %d\nDepth: %d\nNodes: %d\nTime %d', -(score + mate_value) / 2 - 1, current_depth, nodes, new Date().getTime() - start);
-        
-        //else if (score > mate_score && score < mate_value)
-          //console.log('Score: mate in %d\nDepth: %d\nNodes: %d\nTime %d', (mate_value - score) / 2 + 1, current_depth, nodes, new Date().getTime() - start);   
-        
-        //else
-        let info = 'info score cp ' + score + 
-                   ' depth ' + current_depth +
-                   ' nodes ' + nodes +
-                   ' time ' + (new Date().getTime() - start) +
-                   ' pv ';
-
-        for (var count = 0; count < pvLength[0]; count++)
-          info += moveToString(pvTable[count]) + ' ';
-        
-        console.log(info);
-      //}
-    }
     
-    console.log('bestmove ' + moveToString(pvTable[0]) + '\n');    
+    let lastBestMove = 0;
+    
+    // iterative deepening
+    for (let current_depth = 1; current_depth <= depth; current_depth++) {
+      lastBestMove = pvTable[0];
+      score = negamax(-infinity, infinity, current_depth);
+      if (timing.stopped == 1) break;
+      var info = '';
+      if (score > -mateValue && score < -mateScore) {
+        info = 'info score mate ' + (parseInt(-(score + mateValue) / 2 - 1)) + 
+               ' depth ' + current_depth +
+               ' nodes ' + nodes +
+               ' time ' + (new Date().getTime() - start) +
+               ' pv ';
+      
+      } else if (score > mateScore && score < mateValue) {
+        //console.log('+mate')
+        //info = 'mate';
+        info = 'info score mate ' + (parseInt((mateValue - score) / 2 + 1)) + 
+               ' depth ' + current_depth +
+               ' nodes ' + nodes +
+               ' time ' + (new Date().getTime() - start) +
+               ' pv ';
+      
+      } else {
+        info = 'info score cp ' + score + 
+               ' depth ' + current_depth +
+               ' nodes ' + nodes +
+               ' time ' + (new Date().getTime() - start) +
+               ' pv ';
+
+        for (let count = 0; count < pvLength[0]; count++)
+          info += moveToString(pvTable[count]) + ' ';
+      }
+      
+      console.log(info);
+    }
+
+    console.log('bestmove ' + ( (timing.stopped == 1) ? moveToString(lastBestMove):
+                                                        moveToString(pvTable[0])) + '\n');
   }
 
 
@@ -1281,7 +1289,7 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
 
     // generate unique position identifier
     hashKey = generateHashKey();
-    
+
     // init piece list
     initPieceList();
     
@@ -1528,20 +1536,25 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
   
   function debug() {
     // parse position from FEN string
-    setBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ');
+    //setBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ');
     //setBoard('r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 10 ');
     //setBoard('r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10');
     //setBoard('rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8');
     //setBoard('rnbqkbnr/pp4pp/2p5/3Npp2/2PpP3/3P1P2/PP4PP/R1BQKBNR b KQkq e3 0 6 ');
     
     //setBoard('rn2kb1r/pp5p/5n2/2p5/4pN2/111P4/PPP2PPP/R2Q1RK1 w kq - 0 15 ');
-    printBoard();
-    perftTest(4);
-    console.log(searchPly, gamePly);
-    printBoard();
-    perftTest(4);
-    console.log(searchPly, gamePly);
-    printBoard();
+    
+    
+    setBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ');
+    printPieceList();
+    negamax(-50000, 50000, 4);
+    console.log('nodes:', nodes);
+    nodes = 0;
+    printPieceList();
+    
+    //initPieceList();
+    //setBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ');
+    //negamax(-50000, 50000, 4);
     //console.log('nodes:', nodes);
     
     /*var moveList = [];
@@ -1696,8 +1709,8 @@ if (typeof(document) != 'undefined') {
 
   // init engine
   var engine = new Engine();
-  engine.debug();
-  return;
+  //engine.debug();
+  //return;
 
   process.stdin.setEncoding('utf-8');
   console.log('\n  Wukong JS - UCI mode - v' + VERSION + '\n\n');
@@ -1760,7 +1773,7 @@ if (typeof(document) != 'undefined') {
     );
 
     // search position
-    engine.perft(depth);
+    engine.search(depth);
   }
   
   // parse UCI "position" command
