7a7c5004e2f93d054395f922f51291a13af1068a
Maksim Korzh <freesoft.for.people@gmail.com>
2020-12-27 13:42:06 +0200

    fixed background image in firefox

--- wukong.js
+++ wukong.js
@@ -1051,13 +1051,13 @@ var Engine = function(boardSize, lightSquare, darkSquare, selectColor) {
   // mirror PST for black
   const mirrorSquare = [
     a1, b1, c1, d1, e1, f1, g1, h1,    o, o, o, o, o, o, o, o,
-	  a2, b2, c2, d2, e2, f2, g2, h2,    o, o, o, o, o, o, o, o,
-	  a3, b3, c3, d3, e3, f3, g3, h3,    o, o, o, o, o, o, o, o,
-	  a4, b4, c4, d4, e4, f4, g4, h4,    o, o, o, o, o, o, o, o,
-	  a5, b5, c5, d5, e5, f5, g5, h5,    o, o, o, o, o, o, o, o,
-	  a6, b6, c6, d6, e6, f6, g6, h6,    o, o, o, o, o, o, o, o,
-	  a7, b7, c7, d7, e7, f7, g7, h7,    o, o, o, o, o, o, o, o,
-	  a8, b8, c8, d8, e8, f8, g8, h8,    o, o, o, o, o, o, o, o
+    a2, b2, c2, d2, e2, f2, g2, h2,    o, o, o, o, o, o, o, o,
+    a3, b3, c3, d3, e3, f3, g3, h3,    o, o, o, o, o, o, o, o,
+    a4, b4, c4, d4, e4, f4, g4, h4,    o, o, o, o, o, o, o, o,
+    a5, b5, c5, d5, e5, f5, g5, h5,    o, o, o, o, o, o, o, o,
+    a6, b6, c6, d6, e6, f6, g6, h6,    o, o, o, o, o, o, o, o,
+    a7, b7, c7, d7, e7, f7, g7, h7,    o, o, o, o, o, o, o, o,
+    a8, b8, c8, d8, e8, f8, g8, h8,    o, o, o, o, o, o, o, o
   ]
   
   // insufficient material detection
